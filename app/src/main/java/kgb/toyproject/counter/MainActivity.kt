16f5bb7177570295d9da39d87dcf74aa31456267
agfalcon <agfalcon12@gmail.com>
2023-01-28 18:16:13 +0900

    숫자 세기 기능 구현 완료

--- app/src/main/java/kgb/toyproject/counter/MainActivity.kt
+++ app/src/main/java/kgb/toyproject/counter/MainActivity.kt
@@ -2,10 +2,29 @@ package kgb.toyproject.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textViewCounter = findViewById<TextView>(R.id.text_counter)
+        val btnReset = findViewById<Button>(R.id.btn_reset)
+        val btnPlus = findViewById<Button>(R.id.btn_plus)
+
+
+        var number = 0
+
+        btnReset.setOnClickListener{
+            number = 0
+            textViewCounter.text = number.toString()
+        }
+
+        btnPlus.setOnClickListener {
+            number++
+            textViewCounter.text = number.toString()
+        }
     }
 }
